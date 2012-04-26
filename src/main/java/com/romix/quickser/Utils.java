f5017e2ba61088f311e6638e6aa14ea5e2b7d415
romix <romixlev@gmail.com>
2012-04-26 16:27:40 +0200

    Remove unused methods

--- src/main/java/com/romix/quickser/Utils.java
+++ src/main/java/com/romix/quickser/Utils.java
@@ -3,10 +3,9 @@ package com.romix.quickser;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.Comparator;
 
 /**
- * Various utilities used in JDBM
+ * Various utilities
  */
 class Utils {
 
@@ -16,56 +15,6 @@ class Utils {
 	 */
 	static final String EMPTY_STRING = "";
 
-	/**
-	 * Compares comparables. Default comparator for most of java types
-	 */
-	static final Comparator COMPARABLE_COMPARATOR = new Comparator<Comparable>() {
-		public int compare(Comparable o1, Comparable o2) {
-			return o1.compareTo(o2);
-		}
-	};
-
-	static String formatSpaceUsage(long size) {
-		if (size < 1e4)
-			return size + "B";
-		else if (size < 1e7)
-			return "" + Math.round(1D * size / 1024D) + "KB";
-		else if (size < 1e10)
-			return "" + Math.round(1D * size / 1e6) + "MB";
-		else
-			return "" + Math.round(1D * size / 1e9) + "GB";
-	}
-
-	static boolean allZeros(byte[] b) {
-		for (int i = 0; i < b.length; i++) {
-			if (b[i] != 0)
-				return false;
-		}
-		return true;
-	}
-
-	static <E> E max(E e1, E e2, Comparator comp) {
-		if (e1 == null)
-			return e2;
-		if (e2 == null)
-			return e1;
-
-		if (comp == null)
-			comp = COMPARABLE_COMPARATOR;
-		return comp.compare(e1, e2) < 0 ? e2 : e1;
-	}
-
-	static <E> E min(E e1, E e2, Comparator comp) {
-		if (e1 == null)
-			return e2;
-		if (e2 == null)
-			return e1;
-
-		if (comp == null)
-			comp = COMPARABLE_COMPARATOR;
-
-		return comp.compare(e1, e2) > 0 ? e2 : e1;
-	}
 
 	static final Serializer<Object> NULL_SERIALIZER = new Serializer<Object>() {
 		public void serialize(DataOutput out, Object obj) throws IOException {
