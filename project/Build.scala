b7b728390ddbdcd2e4b002c5af59826d4d00c0c0
eedrls <eedrls@EV68B599F60A3C.eemea.ericsson.se>
2012-05-19 15:37:28 +0200

    Add support for SBT builds

---
+++ project/Build.scala
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright 2012 Roman Levenstein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+import sbt._
+import Keys._
+
+object MinimalBuild extends Build {
+
+  lazy val buildVersion = "1.0-SNAPSHOT"
+
+  lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+  lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
+
+  lazy val root = Project(id = "quickser", base = file("."), settings = Project.defaultSettings).settings(
+    version := buildVersion,
+    organization := "com.romix",
+    resolvers += typesafe,
+    resolvers += typesafeSnapshot,
+    publishArtifact in packageDoc := false,
+    // disable using the Scala version in output paths and artifacts
+    crossPaths := false,
+    libraryDependencies += "com.googlecode" % "reflectasm" % "1.01",
+    libraryDependencies += "junit" % "junit" % "4.8.2"
+    )
+}
